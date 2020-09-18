c150bb419bdca5487dfaf185389acb7db6a18776
hyun <hyunjikoh@naver.com>
2020-09-18 10:45:45 +0900

    regularization 추가

--- 1.cifar-10/고현지/fully_conntected_layer.py
+++ 1.cifar-10/고현지/fully_conntected_layer.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.regularizers import l2
 from sklearn.model_selection import train_test_split
 
 
@@ -16,24 +17,24 @@ x_test = x_test.astype('float32')
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
-# train 데이터를 다시 train 데이터와 validation 데이터로 나눔
+# train 데이터를 다시 train 과 validation 으로 나눔
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=0)
 
 # 모델 구성
-model = Sequential() # 하나의 입력 -> 하나의 출
+model = Sequential() # 하나의 입력 -> 하나의 출력
 model.add(Flatten())
-model.add(Dense(units=10, activation='softmax')) # Fully Connected layer 딱 하로 softmax 함수 사용
+model.add(Dense(units=10, activation='softmax',kernel_regularizer=l2(0.01))) # Fully Connected layer 딱 하나로 softmax 함수 사용
 
-# 모델 학습과정 설정
-# 손실 함수 : 다중 분류 손실함수. one-hot encoding 클래스
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# categorical_crossentropy : softmax가 마지막 layer일 대 사용
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 # 모델 학습
 # epochs => 학습사진을 재사용하도록  # batch_size => 한번에 처리하는 사진의 장 수
 hist = model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_val, y_val), shuffle=True)
 
-# 테스트케이스로 모델 검증
+# 테스트케이스로 모델 검증 / evaluate은 ㅣoss와 accuracy 보여주고, predict는 input에 대한 output 예측
 score = model.evaluate(x_test, y_test, verbose=0)
 print('#######################')
 print('Loss: %.3f' % score[0])
 print('Accuracy: %.3f' % score[1])
+
